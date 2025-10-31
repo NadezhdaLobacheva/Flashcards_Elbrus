e2319e53e3425d7d70597e9bc7c181a6a7b20e8d
orrey6three <orstudiocorp@gmail.com>
2025-10-31 13:54:11 +0500

    upd

---
+++ client/src/app/layout/layout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router";
+import "./Layout.css";
+import Header from "../../widgets/Header/Header";
+
+export default function Layout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <footer>Footer</footer>
+    </>
+  );
+}
